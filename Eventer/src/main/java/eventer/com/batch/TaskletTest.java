9cebbfe597a6d952440b21ff64a4993f2014a032
OngSil <66738767+OngSil@users.noreply.github.com>
2023-03-11 17:57:34 +0900

    Spring Batch 환경세팅

---
+++ Eventer/src/main/java/eventer/com/batch/TaskletTest.java
@@ -0,0 +1,10 @@
+package eventer.com.batch;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+
+/*
+public class TaskletTest implements Tasklet {
+	
+}
+*/
